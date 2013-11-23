0c96901f08e24fd36ab573a294536e7f76bff9b0
Shota Fukumori (sora_h) <her@sorah.jp>
2013-11-23 09:40:45 +0900

    Add Akane::Recorder

---
+++ lib/akane/recorder.rb
@@ -0,0 +1,102 @@
+require 'thread'
+
+module Akane
+  class Recorder
+    def initialize(storages, logger: Logger.new(nil))
+      @storages = storages
+      @logger = logger
+      @queue = Queue.new
+      @recently_performed = RoundrobinFlags.new(1000)
+    end
+
+    def queue_length
+      @queue.size
+    end
+
+    def record_tweet(account, tweet)
+      @queue << [:record_tweet, account, tweet]
+      self
+    end
+
+    def mark_as_deleted(account, user_id, tweet_id)
+      @queue << [:mark_as_deleted, account, user_id, tweet_id]
+      self
+    end
+
+    def record_message(account, message)
+      @queue << [:record_message, account, message]
+      self
+    end
+
+    def record_event(account, event)
+      @queue << [:record_event, account, event]
+      self
+    end
+
+    def dequeue(raise_errors = false)
+      perform(*@queue.pop, raise_errors: raise_errors)
+    end
+
+    def perform(action, account, *payload, raise_errors: false)
+      if action == :record_tweet
+        return if @recently_performed[payload.last["id"]]
+        @recently_performed.flag!(payload.last["id"])
+
+        if payload.last["retweeted_status"]
+          perform(:record_tweet, account, payload.last["retweeted_status"], raise_errors: raise_errors)
+        end
+      end
+
+      @storages.each do |storage|
+        begin
+          storage.__send__(action, account, *payload)
+        rescue Exception => e
+          raise e if raise_errors
+          @logger.error "Error while recorder performing to #{storage.inspect}:  #{e.inspect}"
+          @logger.error e.backtrace
+        end
+      end
+    end
+
+    def run(raise_errors = false)
+      loop do
+        begin
+          self.dequeue(raise_errors)
+        rescue Exception => e
+          raise e if raise_errors
+          @logger.error "Error while recorder dequing: #{e.inspect}"
+          @logger.error e.backtrace
+        end
+      end
+    end
+
+    class RoundrobinFlags
+      def initialize(size)
+        @hash = {}
+        @limit_size = size
+      end
+
+      def [](k)
+        @hash[k]
+      end
+
+      def flag!(k)
+        @hash[k] = true
+        if @limit_size < @hash.size
+          @hash.keys.first(@hash.size-@limit_size).each do |overflowed_key|
+            @hash.delete overflowed_key
+          end
+        end
+        nil
+      end
+
+      def unflag!(k)
+        @hash.delete k
+      end
+
+      def flags
+        @hash.keys
+      end
+    end
+  end
+end
---
+++ spec/recorder_spec.rb
@@ -0,0 +1,86 @@
+require 'spec_helper'
+require 'akane/recorder'
+
+describe Akane::Recorder do
+  let(:storages) do
+    [
+      double("storage0")
+    ]
+  end
+  subject { described_class.new(storages) }
+
+  describe "recording tweets" do
+    it "records tweet" do
+      storages[0].should_receive(:record_tweet).with('a', {"id" => 42})
+      subject.record_tweet('a', "id" => 42)
+      subject.dequeue(true)
+    end
+
+    it "doesn't record tweets which already recorded recently" do
+      storages[0].should_receive(:record_tweet).with('a', {"id" => 40})
+      storages[0].should_receive(:record_tweet).with('a', {"id" => 42})
+
+      subject.record_tweet('a', "id" => 40)
+      subject.record_tweet('a', "id" => 42)
+      subject.dequeue(true)
+      subject.dequeue(true)
+      subject.record_tweet('a', "id" => 42)
+      subject.dequeue(true)
+    end
+
+    it "records retweeted tweet" do
+      tweet = {"id" => 42, "text" => "foo", "user" => {"id" => 1, "screen_name" => "a"}}
+      retweet = {"id" => 43, "text" => "RT @a: foo", "user" => {"id" => 2, "screen_name" => "b"}, "retweeted_status" => tweet}
+      storages[0].should_receive(:record_tweet).with('a', retweet)
+      storages[0].should_receive(:record_tweet).with('a', tweet)
+      subject.record_tweet('a', retweet)
+      subject.dequeue(true)
+    end
+  end
+
+  describe "marking deletion" do
+    it "marks as deleted" do
+      subject.mark_as_deleted('foo', 1, 42)
+
+      storages[0].should_receive(:mark_as_deleted).with('foo', 1, 42)
+      subject.dequeue(true)
+    end
+  end
+
+  describe "recording messages" do
+    it "records message" do
+      subject.record_message('foo', {"id" => 42})
+
+      storages[0].should_receive(:record_message).with('foo', "id" => 42)
+      subject.dequeue(true)
+    end
+  end
+
+  describe "recording event" do
+    it "records event" do
+      subject.record_event('foo', {"event" => "favorite"})
+
+      storages[0].should_receive(:record_event).with('foo', "event" => "favorite")
+      subject.dequeue(true)
+    end
+  end
+
+  describe "#run" do
+    before do
+      @th = Thread.new { subject.run(true) }
+      @th.abort_on_exception = true
+    end
+
+    it "continues dequeuing the queue" do
+      storages[0].should_receive(:record_tweet).with('a', {"id" => 42})
+      storages[0].should_receive(:record_tweet).with('b', {"id" => 43})
+      subject.record_tweet('a', "id" => 42)
+      subject.record_tweet('b', "id" => 43)
+      10.times { break if subject.queue_length.zero?; sleep 0.1 }
+    end
+
+    after do
+      @th.kill if @th && @th.alive?
+    end
+  end
+end
