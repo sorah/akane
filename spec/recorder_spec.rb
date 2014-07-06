0d741e4786df470ac7aee8f01ca06101a1e9be7d
Shota Fukumori (sora_h) <her@sorah.jp>
2014-07-07 04:19:48 +0900

    Fix timing problem; spec might fail

--- spec/recorder_spec.rb
+++ spec/recorder_spec.rb
@@ -66,20 +66,19 @@ describe Akane::Recorder do
   end
 
   describe "#run" do
-    before do
-      @th = Thread.new { subject.run(true) }
-      @th.abort_on_exception = true
-    end
-
     it "continues dequeuing the queue" do
       storages[0].should_receive(:record_tweet).with('a', {:id => 42})
       storages[0].should_receive(:record_tweet).with('b', {:id => 43})
+
+      @th = Thread.new { subject.run(true) }
+      @th.abort_on_exception = true
+
+      15.times { break if @th.status == "sleep"; sleep 0.1 }
+
       subject.record_tweet('a', :id => 42)
       subject.record_tweet('b', :id => 43)
-      10.times { break if subject.queue_length.zero?; sleep 0.1 }
-    end
 
-    after do
+      15.times { break if subject.queue_length.zero?; sleep 0.1 }
       @th.kill if @th && @th.alive?
     end
   end
