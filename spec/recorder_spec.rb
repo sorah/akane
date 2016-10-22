2c5099e4c5022f349a119331327e6f293ce48a83
Sorah Fukumori <her@sorah.jp>
2016-10-22 23:28:37 +0900

    fix test

--- spec/recorder_spec.rb
+++ spec/recorder_spec.rb
@@ -11,26 +11,29 @@ describe Akane::Recorder do
 
   describe "recording tweets" do
     it "records tweet" do
-      storages[0].should_receive(:record_tweet).with('a', {id: 42})
-      subject.record_tweet('a', id: 42)
+      tweet = Twitter::Tweet.new(id: 42)
+      storages[0].should_receive(:record_tweet).with('a', tweet)
+      subject.record_tweet('a', tweet)
       subject.dequeue(true)
     end
 
     it "doesn't record tweets which already recorded recently" do
-      storages[0].should_receive(:record_tweet).with('a', {id: 40})
-      storages[0].should_receive(:record_tweet).with('a', {id: 42})
+      tweet0 = Twitter::Tweet.new(id: 40)
+      tweet1 = Twitter::Tweet.new(id: 42)
+      storages[0].should_receive(:record_tweet).with('a', tweet0)
+      storages[0].should_receive(:record_tweet).with('a', tweet1)
 
-      subject.record_tweet('a', id: 40)
-      subject.record_tweet('a', id: 42)
+      subject.record_tweet('a', tweet0)
+      subject.record_tweet('a', tweet1)
       subject.dequeue(true)
       subject.dequeue(true)
-      subject.record_tweet('a', id: 42)
+      subject.record_tweet('a', tweet1)
       subject.dequeue(true)
     end
 
     it "records retweeted tweet" do
-      tweet = {id: 42, text: "foo", user: {id: 1, screen_name: "a"}}
-      retweet = {id: 43, text: "RT @a: foo", user: {id: 2, screen_name: "b"}, retweeted_status: tweet}
+      tweet = Twitter::Tweet.new(id: 42, text: "foo", user: {id: 1, screen_name: "a"})
+      retweet = Twitter::Tweet.new(id: 43, text: "RT @a: foo", user: {id: 2, screen_name: "b"}, retweeted_status: tweet.to_hash)
       storages[0].should_receive(:record_tweet).with('a', retweet)
       storages[0].should_receive(:record_tweet).with('a', tweet)
       subject.record_tweet('a', retweet)
@@ -67,16 +70,19 @@ describe Akane::Recorder do
 
   describe "#run" do
     it "continues dequeuing the queue" do
-      storages[0].should_receive(:record_tweet).with('a', {:id => 42})
-      storages[0].should_receive(:record_tweet).with('b', {:id => 43})
+      tweet0 = Twitter::Tweet.new(id: 42)
+      tweet1 = Twitter::Tweet.new(id: 43)
+
+      storages[0].should_receive(:record_tweet).with('a', tweet0)
+      storages[0].should_receive(:record_tweet).with('b', tweet1)
 
       @th = Thread.new { subject.run(true) }
       @th.abort_on_exception = true
 
       15.times { break if @th.status == "sleep"; sleep 0.1 }
 
-      subject.record_tweet('a', :id => 42)
-      subject.record_tweet('b', :id => 43)
+      subject.record_tweet('a', tweet0)
+      subject.record_tweet('b', tweet1)
 
       15.times { break if subject.queue_length.zero?; sleep 0.1 }
       @th.kill if @th && @th.alive?
@@ -85,16 +91,18 @@ describe Akane::Recorder do
 
   describe "#stop!" do
     it "stops gracefully" do
-      storages[0].should_receive(:record_tweet).with('a', {:id => 42})
+      tweet = Twitter::Tweet.new(id: 40)
+
+      storages[0].should_receive(:record_tweet).with('a', tweet)
       allow(storages[0]).to receive(:exitable?).and_return(true)
 
       @th = Thread.new { subject.run(true) }
       @th.abort_on_exception = true
       15.times { break if @th.status == "sleep"; sleep 0.1 }
 
-      subject.record_tweet('a', :id => 42)
+      subject.record_tweet('a', tweet)
       subject.stop!
-      subject.record_tweet('b', :id => 42)
+      subject.record_tweet('b', tweet)
 
       15.times { break unless @th.alive?; sleep 0.1 }
 
